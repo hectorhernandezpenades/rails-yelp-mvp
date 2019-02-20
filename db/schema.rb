e0ee2bd6e9457988570c1ba66349db5832241ebf
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-02-20 16:32:08 +0000

    controllers, models, routes and db working, views need to be done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_130626) do
+ActiveRecord::Schema.define(version: 2019_02_20_145321) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "category"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '07851857680',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07851857680',
+    category:     'italian'
+  },
+  {
+    name:         'Pizza Union',
+    address:      '32 Kings Cross, London',
+    phone_number: '07851857680',
+    category:     'italian'
+  },
+  {
+    name:         'Five Guys',
+    address:      'Picadilly Circus, London E1 6PQ',
+    phone_number: '07851857680',
+    category:     'french'
+  },
+  {
+    name:         'Misato',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '07851857680',
+    category:     'japanese'
+  },
+  {
+    name:         'Red Dog Saloon',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07851857680',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
